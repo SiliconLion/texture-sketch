9fd579ea0aa0bca9609693717121f38d4d2bd1d9
David <touisbetterthanpi@gmail.com>
2020-04-25 09:30:15 -0500

    Refactor: Moved vertex stuff to geometry class. Fixed bug
    -The geometry class had old code, but we just deleted it all and moved the working code from main.cpp to there.
    - fixed the bug where the viewport is incorrectly sized until you resize the frame.

--- geometry.h
+++ geometry.h
@@ -12,114 +12,114 @@ using std::memcpy;
 
 class Geometry {
 public:
-    //the vertices are in the format x, y, tX, tY
-    //x and y are the x and y positions in space. 
-    //tX and tY are the texture coordinates
-    float* data;
-    //number of verticies currently in data
-    int vertexCount;
-    //number of verticies data can hold without being resized
-    int vertexCapacity;
-    //the number of floats long a vertex is
-    const unsigned int VERTEXLEN = 4;
-    //the size in bytes a vertex is
-    const unsigned long VERTEXSIZE = sizeof(float) * VERTEXLEN;
+    //number of bytes in a vertex.
+    const int VERTEX_SIZE = 4 * sizeof(float);
+    //number of bytes in an index. (mostly just for consistency)
+    const int INDEX_SIZE = sizeof(unsigned int);
+    //number of verticies currently in verticies.
+    int vertex_count = 4;
+    
+    float verticies[16]  = {
+            // positions            // texture coords
+             0.5f,  0.5f,        1.0,  1.0,
+             0.5f, -0.5f,        1.0,  0.0,
+            -0.5f, -0.5f,        0.0,  0.0,
+            -0.5f,  0.5f,        0.0,  1.0
+        }; 
+    //the number of indicies currently in indicies
+    int indicies_count = 6;
+    
+    unsigned int indicies[6] = {  
+            0, 1, 3,  
+            1, 2, 3   
+        };
 
-    unsigned int * indicies;
 
-    //the vertex array object. Stores 
     unsigned int VAO;
-    //the vertex buffer object
     unsigned int VBO;
-    //element buffer object
     unsigned int EBO;
 
     Geometry() {
-        vertexCount = 0;
-        vertexCapacity = 1000;
-        data = (float*)calloc(vertexCapacity, VERTEXSIZE);
-        indicies = (unsigned int*)calloc(vertexCapacity, sizeof(unsigned int));
 
-
-        float firstVerts[12] = {-0.5, -0.5, -0.5, -0.5,
-                                 0.0,  0.5,  0.0,  0.5,
-                                 0.5, -0.5,  0.5, -0.5};
-
-        memccpy(data, firstVerts, 12, sizeof(float));
-
-        unsigned int firstIndicies[3] = {0,1,2};
-        memccpy(indicies, firstIndicies, vertexCount, sizeof(unsigned int));
-
-        //bind the VAO so subsequent calls can be bound with just the VAO
         glGenVertexArrays(1, &VAO);
         glBindVertexArray(VAO);
 
-        errorAtLine_GL();
-        //generate and bind the VBO
+        
         glGenBuffers(1, &VBO);
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, VERTEXSIZE * vertexCount, data, GL_DYNAMIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, vertex_count * VERTEX_SIZE, verticies, GL_STATIC_DRAW);
 
-        errorAtLine_GL();
-
+        
         glGenBuffers(1, &EBO);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * vertexCount, indicies, GL_DYNAMIC_DRAW);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicies_count * INDEX_SIZE, indicies, GL_STATIC_DRAW);
 
-        errorAtLine_GL();
-        //attribute 0 is x and y
-        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, VERTEX_SIZE, (void*)0);
         glEnableVertexAttribArray(0);
-        errorAtLine_GL();
-        //attribute 1 is tX and tY
-        glVertexAttribPointer(1, 2 , GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)) );
+
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, VERTEX_SIZE, (void*)(2 * sizeof(float)) );
         glEnableVertexAttribArray(1);
 
-        errorAtLine_GL();
-
-        printf("gl errors in geometry intitialization: %x\n", glGetError());
-
-        // unbinds everything
+        //unbind everything
+        glBindVertexArray(0);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-        glBindVertexArray(0);
     }
 
-    void addVertex(float x, float y, float tx, float ty) {
-        if(vertexCount == vertexCapacity) {
-            data = (float*)realloc(data, vertexCapacity * 2 * VERTEXSIZE);
-            vertexCapacity *=2;
-        }
-
-        // memcpy(data + (vertexCount * VERTEXLEN), vertex, VERTEXLEN);
-        data[VERTEXLEN * vertexCount + 1] = x;
-        data[VERTEXLEN * vertexCount + 2] = y;
-        data[VERTEXLEN * vertexCount + 3] = tx;
-        data[VERTEXLEN * vertexCount + 4] = ty;
-        vertexCount += 1;
-
-
-
+    void bind() {
         glBindVertexArray(VAO);
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, VERTEXSIZE * vertexCount, data, GL_DYNAMIC_DRAW);
-
-        //unbinds everything
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    }
+    void unbind() {
         glBindVertexArray(0);
-
-        printf("gl errors in Geometry::addVertex : %x\n", glGetError());
     }
 
+    //assumes a shader program has already been bound
     void draw() {
-        glBindVertexArray(VAO);
-        errorAtLine_GL();
-        // glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        // errorAtLine_GL();
-        // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-        // errorAtLine_GL();
-        glDrawArrays(GL_TRIANGLES, 0, vertexCount);
-        // glDrawElements(GL_TRIANGLE_STRIP, vertexCount, GL_UNSIGNED_INT, )
-        printf("gl errors in Geometry::draw : %x\n", glGetError());
+        this->bind();
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        this->unbind();
     }
 };
+
+
+
+    
+    // int vertex_count = 4;
+    // float verticies[] = {
+    // // positions            // texture coords
+    //  0.5f,  0.5f,        1.0,  1.0,
+    //  0.5f, -0.5f,        1.0,  0.0,
+    // -0.5f, -0.5f,        0.0,  0.0,
+    // -0.5f,  0.5f,        0.0,  1.0
+    // };
+    
+    
+    // int indicies_len = 6;
+    // unsigned int indicies[] = {  
+    //     0, 1, 3,  
+    //     1, 2, 3   
+    // }; 
+
+
+    // unsigned int VAO;
+    // glGenVertexArrays(1, &VAO);
+    // glBindVertexArray(VAO);
+
+    // unsigned int VBO;
+    // glGenBuffers(1, &VBO);
+    // glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    // glBufferData(GL_ARRAY_BUFFER, vertex_count * VERTEX_SIZE, verticies, GL_STATIC_DRAW);
+
+    // unsigned int EBO;
+    // glGenBuffers(1, &EBO);
+    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicies_len * INDEX_SIZE, indicies, GL_STATIC_DRAW);
+
+
+    // glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, VERTEX_SIZE, (void*)0);
+    // glEnableVertexAttribArray(0);
+
+    // glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, VERTEX_SIZE, (void*)(2 * sizeof(float)) );
+    // glEnableVertexAttribArray(1);
--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@
 #include "stb_image.h"
 
 #include "utilities.h"
+#include "geometry.h"
 
 //updates the glViewport when the window is resized.
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
@@ -46,51 +47,14 @@ int main()
         glfwTerminate();
         return -1;
     }
-    glfwMakeContextCurrent(window);
     glViewport(0, 0, 800, 600);
+    glfwMakeContextCurrent(window);
 
     //adds the callback we defined above to a change in framebuffer size
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 
-    std::cout << __LINE__ << std::endl;
-
-    float verticies[] = {
-    // positions            // texture coords
-     0.5f,  0.5f,        1.0,  1.0,
-     0.5f, -0.5f,        1.0,  0.0,
-    -0.5f, -0.5f,        0.0,  0.0,
-    -0.5f,  0.5f,        0.0,  1.0
-    };
-    
-    unsigned int indicies[] = {  // note that we start from 0!
-        0, 1, 3,   // first triangle
-        1, 2, 3    // second triangle
-    }; 
-
-
-    unsigned int VAO;
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
-
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
-
-    unsigned int EBO;
-    glGenBuffers(1, &EBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
-
-
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)) );
-    glEnableVertexAttribArray(1);
-
-
+    Geometry geom = Geometry();
 
 
 
@@ -181,9 +145,7 @@ int main()
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture);
 
-        glBindVertexArray(VAO);
-        // glDrawArrays(GL_TRIANGLES, 0, 3);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        geom.draw();
 
 
 
